9806d6c7ea9e8e9039ac9782a8555ca0a9ec4bd3
menixator <ahmed.miljau@gmail.com>
2017-12-06 09:31:53 +0500

    Whoops. Forgot to remove mock graph data

--- src/components/Dashboard/Graph.js
+++ src/components/Dashboard/Graph.js
@@ -18,15 +18,6 @@ delete colors.black;
 
 const COLORS = [colors.red[500], colors.blue[500], colors.green[400], colors.purple[500]];
 
-const mockData = [
-  { name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
-  { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
-  { name: "Page C", uv: 2000, pv: 9800, amt: 2290 },
-  { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
-  { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
-  { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
-  { name: "Page G", uv: 3490, pv: 4300, amt: 2100 }
-];
 
 class CustomTooltip extends React.Component {
   propTypes: {
